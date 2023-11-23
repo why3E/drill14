2ec0fd90a9dc34ba1086be3f3dd05455d0f2f5c4
wonju <jwwon00@naver.com>
2023-11-23 10:55:06 +0900

    drill14 제출 2019182025 원정우

--- ball.py
+++ ball.py
@@ -12,7 +12,7 @@ class Ball:
     def __init__(self, x = None, y = None):
         if Ball.image == None:
             Ball.image = load_image('ball21x21.png')
-        self.x = x if x else random.randint(100, 1180)
+        self.x = x if x else random.randint(100, 1500)
         self.y = y if y else random.randint(100, 924)
 
     def draw(self):
